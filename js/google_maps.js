af8fdee42bbd2ecc86fa0d2c1a16450947614c4a
Lucas Hourquebie <=>
2016-12-03 15:50:25 -0300

    Basic statistics added

--- js/google_maps.js
+++ js/google_maps.js
@@ -4,8 +4,13 @@ function initMap() {
     dataType: 'json',
     success: function(locations){
       if (locations != "-1") {
+
+        var distance = 0;
+        var start = new Date(locations[0].registered_at);
+        var end = new Date(locations[locations.length - 1].registered_at);
+
         var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 14,
+          zoom: 15,
           center: new google.maps.LatLng(locations[0].latitude, locations[0].longitude),
           mapTypeId: google.maps.MapTypeId.ROADMAP
         });
@@ -15,14 +20,31 @@ function initMap() {
 
         for (i = 0; i < locations.length; i++) {
           marker = new google.maps.Marker({
-            position: new google.maps.LatLng(locations[i].latitude, locations[i].longitude), map: map, title: "Marcador" });
+            position: new google.maps.LatLng(locations[i].latitude, locations[i].longitude),
+            map: map
+          });
           google.maps.event.addListener(marker, 'click', (function(marker, i) {
             return function() {
-              infowindow.setContent(locations[i][0]);
+              infowindow.setContent('<div><b>Fecha y hora:</b></div> ' + '<div>' + locations[i].registered_at + '</div>');
               infowindow.open(map, marker);
             }
           })(marker, i));
+
+          if (i > 0){
+            previous_position = new google.maps.LatLng(locations[0].latitude, locations[0].longitude);
+            distance += google.maps.geometry.spherical.computeDistanceBetween (previous_position, marker.position);
+          }
         }
+
+        /* Distancia */
+        $('#js-distance').data('distance_in_mts', distance);
+        $('#js-distance').text((Math.round(distance*100) / 100).toString() + " mts");
+
+        /* Tiempo */
+        seconds = Math.abs(start - end) / (1000);
+        $('#js-time').data('time_in_secs', seconds);
+        $('#js-time').text((Math.round(seconds*100/60)/100).toString() + " minutos");
+
       }
     }
   });
--- map.php
+++ map.php
@@ -7,7 +7,7 @@
 
     <body>
 
-      <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBXg61_WRZBet7yTi_x1z8RRDtWJUNYZ90&callback=initMap" async defer></script>
+      <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyBXg61_WRZBet7yTi_x1z8RRDtWJUNYZ90&callback=initMap&libraries=geometry" async defer></script>
 
       <?php include_once("header.php"); ?>
 
@@ -15,6 +15,32 @@
         <div id="container">
           <div id="map"></div>
         </div>
+        <div class="statistics-panel text-center">
+          <div class="statistics-panel-header">
+             Estadísticas
+          </div>
+          <div class="statistics-panel-body">
+            <div class="text-bold">
+              Distancia recorrida
+            </div>
+            <div id="js-distance" data-distance_in_mts="">
+              ---
+            </div>
+            <div class="text-bold">
+              Tiempo transcurrido
+            </div>
+            <div id="js-time" data-time_in_secs="">
+              ---
+            </div>
+            <div class="text-bold">
+              Velocidad promedio
+            </div>
+            <div class="">
+              ---
+            </div>
+          </div>
+        </div>
       </div>
+
     </body>
 </html>
